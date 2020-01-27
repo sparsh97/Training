91aa459b680ca10056a4e256046116287e22406e
Sparsh <sparshvermadmc@gmail.com>
2020-01-27 20:20:52 +0530

    some changes in the todo file

--- JQuery/Todo List/todo.js
+++ JQuery/Todo List/todo.js
@@ -1,10 +1,16 @@
-$(dosument).rady(function(){
-   $('#addItem').on('click',completeItem);
-   $('#todos').on('change','.completeItem',completeItem);
-   $('#todos').on('click', '.deleteItem', deleteItem);
-
+$(document).ready(function(){
+   $("#addItem").on('click',addItem);
    function addItem(event){
-      var newItem=$("#newTodo").val();
-      $('#todos').append('<li><input class="complet')
+      var newItem=$('#newTodo').val();
+      $("#todos").append('<li><input class="completeItem" type="checkbox">'+ newItem+ '<span class="glyphicon glyphicon-trash deleteItem" aria-hidden="true"></span></li>');
+      $('#newTodo').val(""); // to remove the input area text
+   }
+   $("#todos").on('click','.deleteItem',deleteItem);
+   function deleteItem(event){
+      $(this).parent().remove()
+   }
+   $('#todos').on('click','.completeItem',completeItem);
+   function completeItem(){
+      $(this).parent().toggleClass('done');
    }
 })
