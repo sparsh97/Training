89e1357938559ca6f9761c10a5ffc022d8d3f09f
Sparsh Verma <INT163@xceedance.com>
2020-01-28 14:00:39 +0530

    js added

--- Shopping Cart/shop.js
+++ Shopping Cart/shop.js
@@ -9,82 +9,63 @@ var cart=$('#cart');
 var item;
 var price=0;
 var totalPrice=0;
-   
-   // mobile.on('click',addItem);
-   mobile.click(function(){
+   mobile.click(function(event){
+      event.preventDefault();
       cart.removeClass('d-none');
        item=$('#newTodo1').html();
        price=$('#price1').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem ml-4"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
-      
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
    });
-   // function addItem(event){
-   //     cart.removeClass('d-none');
-   //     item=$('#newTodo1').html();
-   //     price=$('#price1').html();
-   //    $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-   //     item=$('#newTodo1').html('');
-   //     price=$('#price1').html('');
-   // }
-   mTshirt.click(function(){
+   mTshirt.click(function(event){
+      event.preventDefault();
       cart.removeClass('d-none');
        item=$('#newTodo2').html();
        price=$('#price2').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
    });
-   fTop.click(function(){
+   fTop.click(function(event){
+      event.preventDefault();
       cart.removeClass('d-none');
        item=$('#newTodo3').html();
        price=$('#price3').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
    });
-   ac.click(function(){
-      cart.removeClass('d-none');
+   ac.click(function(event){
+       event.preventDefault();
+       cart.removeClass('d-none');
        item=$('#newTodo4').html();
        price=$('#price4').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
    });
-   footWear.click(function(){
-      cart.removeClass('d-none');
+   footWear.click(function(event){
+       event.preventDefault();
+       cart.removeClass('d-none');
        item=$('#newTodo5').html();
        price=$('#price5').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
    });
-   tv.click(function(){
-      cart.removeClass('d-none');
+   tv.click(function(event){
+       event.preventDefault();
+       cart.removeClass('d-none');
        item=$('#newTodo6').html();
        price=$('#price6').html();
        totalPrice+=Number(price);
-      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp   '+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
-      //  item=$('#newTodo1').html('');
-      //  price=$('#price1').html('');
+      $("#todos").append('<li class="display-4">'+ item+'&nbsp &nbsp'+price+ '<span class="deleteItem"><i class="fas fa-trash-alt"></i></span></li>');
       $('#totalPrice').text(totalPrice);
    });
    $("#todos").on('click','.deleteItem',deleteItem);
    function deleteItem(event){
-      $(this).parent().remove()
-      //The parent() method returns the direct parent element of the selected element.
+      $(this).parent().remove();
+      // totalPrice=toatalPrice-;
    }
-   // $('#totalPrice').text(totalPrice);
-   // $('#totalPrice').text
 });
