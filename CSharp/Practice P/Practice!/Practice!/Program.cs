566e25a82d29f0a40fc54a7423a62a68e92b93ae
Sparsh <sparshvermadmc@gmail.com>
2020-02-02 20:44:08 +0530

    Folder Created for csharp

---
+++ CSharp/Practice P/Practice!/Practice!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
