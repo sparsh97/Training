ae15089296ccf0dc3ce4770c51c45e386fc431bf
Sparsh Verma <sparshvermadmc@gmail.com>
2020-02-04 16:23:28 +0530

    calculator

---
+++ ConsoleApp2/Address/Modification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Address
+{
+    class Modification
+    {
+        public void modify()
+        {
+            Console.WriteLine("Modify"); 
+        }
+        public void add()
+        {
+            Console.WriteLine("Added the address");
+        }
+        public void delete()
+        {
+            Console.WriteLine("Delete the address");
+        }
+        public void quit()
+        {
+            Console.WriteLine("Item Deleted");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp2.Address;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Address: ");
+            string name = Console.ReadLine();
+            char ch = Convert.ToChar(Console.ReadLine());
+            Modification m = new Modification();
+            switch (ch)
+            {
+                case 'm': m.modify();
+                    break;
+                case 'a':
+                    m.add();
+                    break;
+                case 'd':
+                    m.delete();
+                    break;
+                case 'q':
+                    m.quit();
+                    break;
+                default:Console.WriteLine("switch ended");
+                    break;
+            }
+        }
+    }
+}
