c04fcaa4bdbe12f4c549c8685bc0635e94a0a31b
Sparsh Verma <sparshvermadmc@gmail.com>
2020-02-07 09:15:10 +0530

    files

--- CRM/mainClass.cs
+++ CRM/mainClass.cs
@@ -8,8 +8,21 @@ namespace CRM
     {
         public static void Main(string[] args)
         {
-            //Order order = new Order(25);
-            //order.Retrieve(25);
+            Order order = new Order();
+            OrderItem orditem = new OrderItem();
+            OrderRepository or = new OrderRepository();
+            OrderDisplay od = new OrderDisplay();
+            OrderDisplayItem odi = new OrderDisplayItem();
+            Console.WriteLine(or.Retrieve(10)); //or.Retrieve(10);
+            List<Order> lOrder = new List<Order>();
+            lOrder.Add(or.RetrieveOrderDisplay(10));
+            foreach (OrderRepository orr in lOrder)
+            {
+                Console.WriteLine(orr.OrderId);
+                Console.WriteLine(orr.OrderDate);
+                Console.WriteLine(orr.orderItems);
+            }
+            Console.WriteLine(or.RetrieveOrderDisplay(10));
         }
     }
 }
