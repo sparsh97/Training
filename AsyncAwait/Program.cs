d1a2b338dd21392884809628188db4da223a7c99
Sparsh Verma <sparshvermadmc@gmail.com>
2020-02-10 16:03:30 +0530

    mvc

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Console.ReadKey();
+        }
+        
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                }
+            });
+        }
+        
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+    }
+}
---
+++ FirstApplication/Controllers/HomeController1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstApplication.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return ($"Hello {name}, ID is: {ID}");
+        }
+    }
+}
